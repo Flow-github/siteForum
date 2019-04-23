053ae69bffc351b15164dd919dba92d6f0bb83e8
CIRET FLORENT <fciret@adgroupe.priv>
2019-04-23 18:20:54 +0200

    fin mise en place du squelette + html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { NavService } from './service/nav.service';
 import { StructureComponent } from './structure/app.structure';
 import { HomeComponent } from './home/app.home';
+import { NavComponent } from './nav/app.nav';
 
 @NgModule({
   declarations: [
     StructureComponent,
     HomeComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [NavService,
+  ],
   bootstrap: [StructureComponent]
 })
 export class AppModule { }
--- src/app/structure/app.structure.ts
+++ src/app/structure/app.structure.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewChecked, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'structure-root',
   templateUrl: './app.structure.html',
   styleUrls: ['./app.structure.scss']
 })
-export class StructureComponent {
+export class StructureComponent implements OnInit, AfterViewChecked {
   
+  private _contentGlobalElement:any;
+  private _contentElement:any;
+
+  constructor(private _domStructure:ElementRef){
+    
+  }
+
+  public ngOnInit():void{
+    this._contentGlobalElement = this._domStructure.nativeElement.children[0].children[0].children[1];
+    this._contentElement = this._domStructure.nativeElement.children[0].children[0].children[1].children[0];
+  }
+
+  public ngAfterViewChecked():void{
+    var stylesGlobalContent:CSSStyleDeclaration = window.getComputedStyle(this._domStructure.nativeElement.children[0].children[0].children[1], null);
+    var globalContentHeight:number = this._contentGlobalElement.clientHeight;
+    var contentHeight:number = this._contentElement.clientHeight;
+    if(globalContentHeight < contentHeight){
+      this._contentGlobalElement.style.height = contentHeight + 'px';
+      this._contentGlobalElement.style.minHeight = contentHeight + 'px';
+    }
+  }
+
 }
