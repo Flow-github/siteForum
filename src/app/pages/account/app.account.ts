fab843934387b752231201fb60e3d5506c1958b8
CIRET FLORENT <fciret@adgroupe.priv>
2019-05-31 16:58:05 +0200

    create account mise en forme

--- src/app/pages/account/app.account.ts
+++ src/app/pages/account/app.account.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
 import { AbstractPage } from '../abstractPage.component';
 import { RouteService } from 'src/app/service/route.service';
 import { RequestService } from 'src/app/service/request.service';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
     selector: '',
@@ -15,6 +15,8 @@ export class AccountComponent extends AbstractPage{
 
     public createAccountForm:FormGroup;
 
+    private _errorsMessage:string;
+
     constructor(private _routeService:RouteService, _elRef:ElementRef, private _requestService:RequestService, private _formBuilder: FormBuilder){
         super(_routeService, _elRef);
     }
@@ -23,12 +25,12 @@ export class AccountComponent extends AbstractPage{
         super.ngOnInit();
 
         this.createAccountForm = this._formBuilder.group({
-            email: ['', [Validators.required, Validators.email]],
-            password: ['', Validators.required ],
-            passwordConfirm: ['', Validators.required ],
-            pseudo: ['', Validators.required ],
-            cgu: ['', Validators.required ],
-        });
+            email: ['', [Validators.required, Validators.email, Validators.maxLength(255)]],
+            password: ['', [Validators.required, Validators.maxLength(255)] ],
+            passwordConfirm: ['', [Validators.required] ],
+            pseudo: ['', [Validators.required, Validators.maxLength(20)] ],
+            cgu: ['', [Validators.required] ],
+        }, {validator: (formTarget:AbstractControl) => {this.checkCheckbox(formTarget)}});
     }
 
     public ngOnDestroy():void{
@@ -36,31 +38,62 @@ export class AccountComponent extends AbstractPage{
     }
 
     public onSubmitForm():void{
+        this._errorsMessage = '';
         if(this.createAccountForm.status == 'INVALID'){
             this.formIsInvalid();
         }else if(this.createAccountForm.status == 'VALID'){
             this.formIsValid();
         }
+        this.displayErrorMessage();
+    }
+
+    private checkCheckbox(formTarget:AbstractControl):void{
+        console.log('checkCheckbox');
+        /*if(formTarget.get('cgu')){
+            console.log(formTarget.get('cgu').value);
+            if(!formTarget.get('cgu').value){
+                formTarget.get('cgu').reset();
+            }
+        }*/
     }
 
     private formIsInvalid():void{
-        var formControl:FormControl;
-        var control:string;
+        let formControl:FormControl;
+        let control:string;
+        let inputValue:any;
         for(control in this.createAccountForm.controls){
             formControl = this.createAccountForm.controls[control] as FormControl;
+            inputValue = formControl.value;
             if((formControl as any).nativeElement != null){
                 if(formControl.valid){
-                    (formControl as any).nativeElement.classList.add('greenBorder');
-                    (formControl as any).nativeElement.classList.remove('redBorder');
+                    if(control != 'cgu'){
+                        (formControl as any).nativeElement.classList.add('greenBorder');
+                        (formControl as any).nativeElement.classList.remove('redBorder');
+                    }
                 }else{
                     (formControl as any).nativeElement.classList.add('redBorder');
                     (formControl as any).nativeElement.classList.remove('greenBorder');
-                }
-            }else{
-                if(control == 'cgu'){
-                    this._displayError.nativeElement.innerHTML = '<span>Vous devez accepter les conditions d\'utilisations</span>';
+                    if(formControl.errors.maxlength){
+                        console.log(formControl.errors);
+                        let nameInput:string = control;
+                        let maxLength:string = formControl.errors.maxlength.requiredLength.toString();
+                        let textError:string = 'Le champ ' + nameInput + ' ne peut comprendre au maximum que ' + maxLength + ' charactéres';
+                        this.addErrorMessage(textError);
+                    }else if(control == 'cgu'){
+                        this.addErrorMessage('Vous devez accepter les conditions d\'utilisations');
+                    }
                 }
             }
+
+            //formControl.reset();
+            /*if(control == 'cgu'){
+                console.log(formControl);
+                formControl.reset();
+                (formControl as any).nativeElement.checked = inputValue;
+            }else{
+                formControl.reset();
+                (formControl as any).nativeElement.value = inputValue;
+            }*/
         }
     }
 
@@ -97,7 +130,15 @@ export class AccountComponent extends AbstractPage{
             (this.createAccountForm.controls['passwordConfirm'] as any).nativeElement.classList.add('redBorder');
         }
 
-        this._displayError.nativeElement.innerHTML = '<span>' + testError + '</span>';
+        this.addErrorMessage(testError);
+    }
+
+    private addErrorMessage(textError:string):void{
+        this._errorsMessage += textError + '<br/>';
+    }
+
+    private displayErrorMessage():void{
+        this._displayError.nativeElement.innerHTML = '<span>' + this._errorsMessage + '</span>';
     }
 
 }
