fa9b5b5faf92d7a10be9bd312a105f816284f830
Flow-github <ideation@hotmail.fr>
2019-06-17 18:31:35 +0200

    remove unused import

--- src/app/service/request.service.ts
+++ src/app/service/request.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { config } from 'src/environments/config.connexion';
+//import { config } from 'src/environments/config.connexion';
 
 @Injectable()
 export class RequestService{
