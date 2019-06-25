267cbae1d1f114d57be9175e38de5c81a62b5402
CIRET FLORENT <fciret@adgroupe.priv>
2019-06-25 17:36:30 +0200

    remove listener

--- src/app/pages/contact/app.contact.ts
+++ src/app/pages/contact/app.contact.ts
@@ -4,6 +4,7 @@ import { RouteService } from 'src/app/service/route.service';
 import { RequestService } from 'src/app/service/request.service';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: '',
@@ -15,6 +16,8 @@ export class ContactComponent extends AbstractPage{
     @ViewChild('displayReturn') _displayReturn:ElementRef;
 
     public contactForm:FormGroup;
+
+    private _subSendMail:Subscription;
     
     constructor(private _routeService:RouteService, _elRef:ElementRef, private _requestService:RequestService, private _formBuilder: FormBuilder){
         super(_routeService, _elRef);
@@ -29,6 +32,14 @@ export class ContactComponent extends AbstractPage{
           mail: ['', [Validators.required, Validators.email]],
           message: ['', Validators.required]
         });
+    }
+
+    public ngOnDestroy():void{
+        super.ngOnDestroy();
+
+        if(this._subSendMail){
+            this._subSendMail.unsubscribe();
+        }
     }
 
     public onSubmitForm():void{
@@ -53,8 +64,8 @@ export class ContactComponent extends AbstractPage{
     }
   
     private formIsValid(){
-        this._requestService.sendMail(this.contactForm.value).subscribe((res:Response) => this.sendMessageHandler(res), (err:HttpErrorResponse) => this.sendMessageErrorHandler(err));;
-        //console.log(this.contactForm.value);
+        this._subSendMail = this._requestService.sendMail(this.contactForm.value).subscribe((res:Response) => this.sendMessageHandler(res), (err:HttpErrorResponse) => this.sendMessageErrorHandler(err));;
+        
         var formControl:FormControl;
         var control:string;
         for(control in this.contactForm.controls){
@@ -84,14 +95,16 @@ export class ContactComponent extends AbstractPage{
     }
   
     private sendMessageHandler(res:Response){
-        //console.log(res);
+        this._subSendMail.unsubscribe();
+
         this._displayReturn.nativeElement.classList.remove('errorContact');
         this._displayReturn.nativeElement.classList.add('succesContact');
         this._displayReturn.nativeElement.innerHTML = '<span>Votre message a bien été envoyé</span>';
     }
   
     private sendMessageErrorHandler(err:HttpErrorResponse){
-        //console.log(err);
+        this._subSendMail.unsubscribe();
+
         this._displayReturn.nativeElement.classList.remove('succesContact');
         this._displayReturn.nativeElement.classList.add('errorContact');
         this._displayReturn.nativeElement.innerHTML = '<span>Une erreure est survenue, veuillez recommencer ultérieurement</span>';
